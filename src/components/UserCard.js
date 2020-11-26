d18551397ad66a297de89d5713c1329534aff80b
Julian Kizeno <julianquiceno20@gmail.com>
2020-11-26 20:45:45 +0100

    gif changed

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,23 +1,24 @@
 import React, {Component} from 'react'
-import logo from '../images/megif.gif'
+import { Card } from 'react-bootstrap'
+import logo from '../images/me.gif'
 import './styles/UserCard.css'
 
 class UserCard extends Component{
     render(){
         return (
             <div className='container'>
-                <section className='container__content'>
-                    <div className='container__content--text'>
+                <Card as='article' className='container__content'>
+                    <Card.Body className='container__content--text'>
                         <p>“ I am a junior web developer focused on advancing my professional career. I am responsible, communicative and I adapt easily to work teams. This is why I am looking for an opportunity to be part of a company. "
                         </p>
-                        <p className='container__content--name-profession'>
+                        <p>
                             <i><strong>Julian Quiceno</strong> ~</i> Frontend Developer
                         </p>
-                    </div>
+                    </Card.Body>
                     <figure className='container__content--fig'>
                         <img src={logo} alt='probando imganes' className='container__content--img'/>
                     </figure>
-                </section>
+                </Card>
             </div>
         )
     }
