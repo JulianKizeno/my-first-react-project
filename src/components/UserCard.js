c7af14665144aef89cf574d5fb68798ef34902b4
Julian Kizeno <julianquiceno20@gmail.com>
2020-11-26 20:19:53 +0100

    react-bootstrap

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, {Component} from 'react'
 import logo from '../images/megif.gif'
 import './styles/UserCard.css'
 
-class UserCard extends React.Component{
+class UserCard extends Component{
     render(){
         return (
             <div className='container'>
