ab643536a0e743ac813cda0cbb238c5f68435c64
Julian Kizeno <julianquiceno20@gmail.com>
2020-11-26 22:10:13 +0100

    props created

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react'
 import { Card } from 'react-bootstrap'
-import logo from '../images/me.gif'
 import './styles/UserCard.css'
 
 class UserCard extends Component{
@@ -9,14 +8,13 @@ class UserCard extends Component{
             <div className='container'>
                 <Card as='article' className='container__content'>
                     <Card.Body className='container__content--text'>
-                        <p>“ I am a junior web developer focused on advancing my professional career. I am responsible, communicative and I adapt easily to work teams. This is why I am looking for an opportunity to be part of a company. "
-                        </p>
+                        <p>{this.props.textCard}</p>
                         <p>
-                            <i><strong>Julian Quiceno</strong> ~</i> Frontend Developer
+                            <i><strong>{this.props.userName}</strong> ~</i> {this.props.profession}
                         </p>
                     </Card.Body>
                     <figure className='container__content--fig'>
-                        <img src={logo} alt='probando imganes' className='container__content--img'/>
+                        <img src={this.props.gifImg} alt={this.props.imgAlt} className='container__content--img'/>
                     </figure>
                 </Card>
             </div>
--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-// import App from './App';
-import UserCard from './components/UserCard';
+import React from "react"
+import ReactDOM from "react-dom"
+import "./index.css"
+import gifImages from './images/me.gif'
+import UserCard from "./components/UserCard"
 
 ReactDOM.render(
-  <React.StrictMode>
-    <UserCard/>
-  </React.StrictMode>,
-  document.getElementById('root')
+  	<React.StrictMode>
+		<UserCard 
+			gifImg = {gifImages}
+			imgAlt = 'images gif'
+			userName = 'Julian Quiceno'
+			profession = 'Frontend Developer'
+			textCard = '“ I am a junior web developer focused on advancing my professional career. I am responsible, communicative and I adapt easily to work teams. This is why I am looking for an opportunity to be part of a company. "'  
+		/>	
+  	</React.StrictMode>,
+  	document.getElementById("root")
 );
-
