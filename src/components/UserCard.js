8a199072f5aed2f72ee38920ec9aa20523f1d4b6
Julian Kizeno <julianquiceno20@gmail.com>
2020-11-28 01:25:23 +0100

    user card modified

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,20 +1,21 @@
 import React, {Component} from 'react'
+
 import { Card } from 'react-bootstrap'
 import './styles/UserCard.css'
 
 class UserCard extends Component{
     render(){
         return (
-            <div className='container'>
-                <Card as='article' className='container__content'>
-                    <Card.Body className='container__content--text'>
+            <div className='containerCard'>
+                <Card as='article' className='containerCard__content'>
+                    <Card.Body className='containerCard__content--text'>
                         <p>{this.props.textCard}</p>
                         <p>
                             <i><strong>{this.props.userName}</strong> ~</i> {this.props.profession}
                         </p>
                     </Card.Body>
-                    <figure className='container__content--fig'>
-                        <img src={this.props.gifImg} alt={this.props.imgAlt} className='container__content--img'/>
+                    <figure className='containerCard__content--fig'>
+                        <img src={this.props.cardImage} alt={this.props.imgAlt} className='containerCard__content--img'/>
                     </figure>
                 </Card>
             </div>
